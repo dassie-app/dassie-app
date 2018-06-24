ee8fca8aa53804fa6efc6f0f56798dc17c145cd2
Kyle Meenehan <kylemeenehan@gmail.com>
2018-06-24 13:56:40 +0200

    added mock login functionality

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -11,13 +11,8 @@ import { MainPage } from '../';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  // The account fields for the login form.
-  // If you're using the username field with or without email, make
-  // sure to add it to the type
-  account: { email: string, password: string } = {
-    email: 'test@example.com',
-    password: 'test'
-  };
+  email: string;
+  password: string;
 
   // Our translated text strings
   private loginErrorString: string;
@@ -34,9 +29,9 @@ export class LoginPage {
 
   // Attempt to login in through our User service
   doLogin() {
-    this.user.login(this.account).subscribe((resp) => {
+    this.user.login({ email: this.email, password: this.password }).then(() => {
       this.navCtrl.push(MainPage);
-    }, (err) => {
+    }).catch(err => {
       this.navCtrl.push(MainPage);
       // Unable to log in
       let toast = this.toastCtrl.create({
--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -1,52 +1,29 @@
-import 'rxjs/add/operator/toPromise';
-
 import { Injectable } from '@angular/core';
-
 import { Api } from '../api/api';
+import { Storage } from '@ionic/storage';
 
-/**
- * Most apps have the concept of a User. This is a simple provider
- * with stubs for login/signup/etc.
- *
- * This User provider makes calls to our API at the `login` and `signup` endpoints.
- *
- * By default, it expects `login` and `signup` to return a JSON object of the shape:
- *
- * ```json
- * {
- *   status: 'success',
- *   user: {
- *     // User fields your app needs, like "id", "name", "email", etc.
- *   }
- * }Ø
- * ```
- *
- * If the `status` field is not `success`, then an error is detected and returned.
- */
 @Injectable()
 export class User {
   _user: any;
 
-  constructor(public api: Api) { }
+  constructor(private api: Api, private storage: Storage) { }
 
   /**
    * Send a POST request to our login endpoint with the data
    * the user entered on the form.
    */
   login(accountInfo: any) {
-    let seq = this.api.post('login', accountInfo).share();
-
-    seq.subscribe((res: any) => {
-      // If the API returned a successful response, mark the user as logged in
-      if (res.status == 'success') {
-        this._loggedIn(res);
-      } else {
-      }
-    }, err => {
-      console.error('ERROR', err);
+    let query = `users?email=${accountInfo.email}&password=${accountInfo.password}`;
+    return new Promise((resolve, reject) => {
+      this.api.get(query).subscribe((result: any[]) => {
+        if(result.length) {
+          this._loggedIn(result[0])
+          resolve();
+        } else {
+          reject();
+        }
+      });
     });
-
-    return seq;
   }
 
   /**
@@ -73,12 +50,14 @@ export class User {
    */
   logout() {
     this._user = null;
+    this.storage.remove('user');
   }
 
   /**
    * Process a login/signup response to store user data
    */
-  _loggedIn(resp) {
-    this._user = resp.user;
+  _loggedIn(user) {
+    this._user = user;
+    this.storage.set('user', user);
   }
 }
